5cf34874f0245b4a55f7259739827bd6264d94ec
jimenaacuna <jimena.a.saavedra@gmail.com>
2018-05-28 03:14:48 -0400

    agregado html-css-js

---
+++ app.js
@@ -0,0 +1,20 @@
+// eleccion del plato
+
+var menu = prompt("hola , que vas a ordenar");
+
+var ceviche;
+var tacosAlPastor;
+var empanadaChilena;
+var hamburguesa;
+
+if (ceviche) {
+    alert("no esta disponible");
+} else if (tacosAlPastor) {
+    alert("tampoco esta disponible");
+} else if ("empanadaChilena") {
+    alert("oh, sigue ordenando");
+} else if ("hamburguesa"){
+    alert("solo hay hamburguesa");
+} else {
+    alert("adios");
+}
